b29c02cc6e4f4591f63876f9b73ad0ee7e1af14a
freemoon99 <soloist132@gmail.com>
2022-12-23 04:29:51 +0900

    refactor: 예외처리 및 코드 수정
    - 중복된 데이터일 경우
    
    docs/README.md ##요구 사항 #동일한 URL을 입력 할 경우 항상 동일한 shortening 결과 값이 나와야 함 #도메인은 localhost 로 처리 : 보유한 도메인이 있으면 사용해도 무관

--- index.js
+++ index.js
@@ -28,25 +28,6 @@ db.connect((err) => {
   console.log('db 연결 성공');
 })
 
-// const base62 = (num) => {
-//   let quotient = num;
-//   let reminder = [];
-//   let codes = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-
-  
-//   while(quotient >= 62){
-//     reminder.push(quotient%62);
-//     quotient = Math.floor(quotient/62);
-//   }
-
-//   quotient = codes[quotient];
-
-//   while (reminder.length > 0){
-//     quotient = quotient + codes[reminder.pop()];
-//   }
-//   return quotient;
-// }
-
 //링크 가져오기
 const getLinks = (req,res) =>{
   let sql = `SELECT * FROM url`;
@@ -66,37 +47,56 @@ app.get('/', (req, res) => {
 
 //post method
 app.post('/inputUrl', (req, res) => {
+  const basic = `http://localhost:3000`;
+  const fullUrl = req.body.fullUrl;
   let shorten = Math.random().toString(36).replace(/[^a-z0-9]/gi,'').substring(2,10);
-  let sql = `INSERT INTO url(fullUrl,shortUrl) VALUES('${Object.values(req.body)}','http://kyu.sh/${shorten}')`
-  db.query(sql, (err, result)=>{
+  let findSql = `SELECT * FROM url WHERE fullUrl = ?`
+  let sql = `INSERT INTO url(fullUrl, shortUrl) VALUES('${fullUrl}','${shorten}')`
+  if(!fullUrl){
+    res.status(400).json({
+      status:'400',
+      message:'ERROR: 데이터가 입력되지 않았습니다.'
+    });
+  }
+  
+  db.query(findSql,[fullUrl],(err,results)=>{
     if(err){
-      res.status(500).json({
-        status:'notok',
-        message:'ERROR1'
-      });
+      console.log(err);
+      res.sendStatus(404);
+    }
+    if(results.length === 0){
+      db.query(sql,(err,res)=>{
+        if(err){
+          res.status(400).json({
+            status:'400',
+            message:'ERROR: 데이터 입력에 오류가 있습니다.'
+          });
+        }
+      })
     } else {
-      res.status(200).json({
-        status:'ok',
-        shortUrl:`http://kyu.sh/${shorten}`,
+      res.status(400).json({
+        status:'400',
+        message:'ERROR: 이미 중복된 값이 들어있습니다.'
       });
     }
   })
 })
 
-//get method : 변환된 주소 받아오기
-app.get('/getUrls',(req,res)=>{
-  let sql = `SELECT * FROM url`;
-  db.query(sql, (err, result)=>{
-    if(err){
-      res.status(500).json({
-        status:'notok',
-        message:'ERROR2'
-      });
-    } else {
-      res.status(200).json(result);
-    }
-  })
-})
+// // get method : 주소 리다이렉트
+// app.get('/:shortUrl',(req,res)=>{
+//   let shortID = req.params.ID;
+//   let sql = `SELECT * FROM url WHERE `;
+//   db.query(sql, (err, result)=>{
+//     if(err){
+//       res.status(500).json({
+//         status:'notok',
+//         message:'ERROR2'
+//       });
+//     } else {
+//       res.status(200).json(result);
+//     }
+//   })
+// })
 
 app.listen(port, ()=> {
   console.log(`서버가 실행됩니다. http://localhost:${port}`);
